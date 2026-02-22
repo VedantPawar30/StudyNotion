745caf3422b41f96794cc8d15ac10353ab164251
VedantPawar30 <pawarvedant968@gmail.com>
2026-02-22 14:24:01 +0530

    frontendLink

--- SERVER/server.js
+++ SERVER/server.js
@@ -9,7 +9,7 @@ require("dotenv").config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:true,
+    origin:"https://studynotion-self-iota.vercel.app/",
     credentials:true,
 }))
 app.use(
