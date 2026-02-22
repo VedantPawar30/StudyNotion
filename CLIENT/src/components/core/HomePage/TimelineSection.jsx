4127287840a4b3108b528cf6f6b05ef74a6a55e0
VedantPawar30 <pawarvedant968@gmail.com>
2026-02-22 14:20:06 +0530

    deploy1

--- CLIENT/src/components/core/HomePage/TimelineSection.jsx
+++ CLIENT/src/components/core/HomePage/TimelineSection.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Logo1 from "../../../assets/TimeLineLogo/logo1.svg";
-import Logo2 from "../../../assets/TimeLineLogo/logo2.svg";
-import Logo3 from "../../../assets/TimeLineLogo/logo3.svg";
-import Logo4 from "../../../assets/TimeLineLogo/logo4.svg";
+import Logo1 from "../../../assets/TimeLineLogo/Logo1.svg";
+import Logo2 from "../../../assets/TimeLineLogo/Logo2.svg";
+import Logo3 from "../../../assets/TimeLineLogo/Logo3.svg";
+import Logo4 from "../../../assets/TimeLineLogo/Logo4.svg";
 import timelineImage from "../../../assets/Images/TimelineImage.png";
 
 function TimelineSection() {
